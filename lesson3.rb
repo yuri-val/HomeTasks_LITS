e934b869aeb66e7a9485c09461b64f2d8347f62f
yuri-val <yuri.valigursky@gmail.com>
2016-08-25 14:50:52 +0300

    fix some bags

--- lesson3.rb
+++ lesson3.rb
@@ -1,23 +1,4 @@
 #  lesson3.rb
-#  
-#  Copyright 2016 Yuri Valigursky <yuri.valigursky@gmail.com>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
 
 def bin_to_dec bin_num
   bin_str = bin_num.to_s
@@ -28,7 +9,7 @@ def bin_to_dec bin_num
   n = 0
   until bin_str == nil do
     l_symb = bin_str[bin_str.size - 1]
-    dec_val = dec_val + l_symb.to_i * (2 ** n);
+    dec_val = dec_val + l_symb.to_i * (2 ** n)
     n = n + 1
     bin_str = bin_str.slice(0, bin_str.size - 1)
   end
