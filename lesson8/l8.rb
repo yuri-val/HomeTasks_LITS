acf4db7d773c4d1d92e195ab82f7db353129bf67
yuri-val <yuri.valigursky@gmail.com>
2016-09-12 19:47:50 +0300

    some fix

--- lesson8/l8.rb
+++ lesson8/l8.rb
@@ -1,43 +1,34 @@
 class GeometricShape
-	@@perimeter
-	@@square
+	@@perimeter = 0
+	@@square = 0
 end
 
 class Circle < GeometricShape
-	@@radius
-	def initialize args = {}
-
+	def set_prop args = {}
+		@@radius = 0
 	end
-	private
-	def perimeter radius
 
-	end
-	def square radius
-
+	def square
+			Math.PI * @@radius ** 2
 	end
 end
 
 class Rectangle < GeometricShape
-	@@side_a
-	@@side_b
-	def initialize args = {}
-
+	def set_prop args = {}
+		prop = args.prop.split ','
+		@@side_a = 0
+		@@side_b = 0
 	end
-	private
-	def perimeter side_a, side_b
-
-	end
-	def square side_a, side_b
-
+	def square
+		@@side_a * @@side_b
 	end
 end
 
 class Triangle < GeometricShape
-	@@side_a
-	@@side_b
-	@@side_c
-	def initialize = {}
-
+	def set_prop args = {}
+		@@side_a = 0
+		@@side_b = 0
+		@@side_c = 0
 	end
 	private
 	def perimeter side_a, side_b, side_c
@@ -48,19 +39,39 @@ class Triangle < GeometricShape
 	end
 end
 
-class FileParser < IO
+class SqureCalculator
+	@file_struct
+	def initialize parser
+		@file_struct = parser.file_struct
+	end
+	def calculate
+		@file_struct.each do |item|
+			puts item.inspect
+		end
+	end
+
+end
+
+class FileParser
 	@file_path
-	@file_struct = []
 	def initialize path_to_file
 		@file_path = path_to_file
 	end
 
-	def parse_file
-		lines = readlines(file_path)
+	def file_struct; return @file_struct; end
+
+	def parse_file log
+		@file_struct = []
+		lines = IO.readlines @file_path
 		lines.each_index do |i|
 			parameters = lines[i].split ':'
-
-			@file_struct << {}
+			unless parameters.size == 2
+					log.add_to_log "wrong data at string ##{i + 1}"
+			end
+			@file_struct << {"line"=> i + 1, "shape"=> parameters[0], "prop"=> parameters[1].chomp!}
+		end
+		@file_struct.sort_by! do |item|
+			item["shape"]
 		end
 	end
 
@@ -69,7 +80,7 @@ end
 class ErrorLog < File
 	@log_file
 	def initialize path_to_log
-		@log_file = open path_to_file, 'a'
+		@log_file = open path_to_log, 'a'
 	end
 
 	def add_to_log msg
@@ -81,3 +92,11 @@ class ErrorLog < File
 	end
 
 end
+
+log = ErrorLog.new "errors.log"
+path_to_file = ARGV[0]
+parser = FileParser.new path_to_file
+parser.parse_file log
+calculator = SqureCalculator.new parser
+calculator.calculate
+log.close_log
